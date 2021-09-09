96ddb5bdb30c324e51184d541c2278be9e8e7bc5
chris De Groef <chris.degroef@gmail.com>
2021-09-09 14:47:38 +0200

    Laravel : fin NavBar

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,9 @@
+
+
+    <div class="vertical">
+        <a class="font p carre black" href="{{ route('roles.index') }}">Roles</a>
+        <a class="font p carre black" href="{{ route('users.index') }}">Users</a>
+        <a class="font p carre black" href="{{ route('articles.index') }}">Articles</a>
+        <a class="font p carre black" href="{{ route('commentaires.index')}}">Commentaires</a>
+    </div>
+
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -5,16 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <title>Document</title>
     
 </head>
-<body>
+<body class="flex">
 
+    <div class="bg-vertical">
+
+    @include('partials.nav')
+
+    </div>
+    <div>
     @yield('content')
     
+    </div>
+
     <script src="{{ asset('js/app.js') }}"></script>
+
 
 </body>
 </html>
